8cecd6013ad8d55d1a112cd557d1c9038cfd12ce
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-09-26 11:57:22 +0700

    update: fix glitch line on polygon

--- src/views/Landing.js
+++ src/views/Landing.js
@@ -34,8 +34,7 @@ export default function Landing() {
 					</div>
 					<div
 						className="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px"
-						style={{ transform: "translateZ(0)" }}
-					>
+						style={{ transform: "translateZ(0)", marginBottom: "-1px" }}>
 						<svg
 							className="absolute bottom-0 overflow-hidden"
 							xmlns="http://www.w3.org/2000/svg"
@@ -43,8 +42,7 @@ export default function Landing() {
 							version="1.1"
 							viewBox="0 0 2560 100"
 							x="0"
-							y="0"
-						>
+							y="0">
 							<polygon
 								className="text-blueGray-200 fill-current"
 								points="2560 0 2560 100 0 100"
