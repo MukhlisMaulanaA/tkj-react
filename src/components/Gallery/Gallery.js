c3d02e74da666e43098ea0c5d28e19384b4c119d
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-09-30 19:19:01 +0700

    update: fill content to gallery's section

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -5,6 +5,7 @@ import image2 from "assets/img/gallery2.png";
 import image3 from "assets/img/gallery3.png";
 import image4 from "assets/img/gallery4.png";
 import image5 from "assets/img/gallery5-resize.png";
+import image6 from "assets/img/gallery6.png";
 
 const MasonryGridGallery = () => {
 	return (
@@ -18,53 +19,105 @@ const MasonryGridGallery = () => {
 							alt="gallery-photo"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
-            <div className="w-full text-sm absolute text-white bottom-0 p-4">
-							<h3 className="font-bold bottom-0 text-left">PT. Honda Precision Parts Manufacturing</h3>
-              <p>Pembuatan Akrilik</p>
-            </div>
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Honda Precision Parts Manufacturing
+								</h3>
+								<p>Pembuatan Akrilik</p>
+							</div>
 						</div>
 					</div>
 				</div>
-				<div>
-					<img
-						className="h-auto max-w-full rounded-lg object-cover object-center "
-						src={image2}
-						alt="gallery-photo"
-					/>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image2}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Honda Precision Parts Manufacturing
+								</h3>
+								<p>Pembuatan Akrilik</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className="grid gap-4">
-				<div>
-					<img
-						className="h-auto max-w-full rounded-lg object-cover object-center"
-						src={image5}
-						alt="gallery-photo"
-					/>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image5}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>Pas Box</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className="flex flex-col justify-between">
-				<div>
-					<img
-						className="h-auto max-w-full rounded-lg object-cover object-center"
-						src={image4}
-						alt="gallery-photo"
-					/>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image4}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>AHU</p>
+							</div>
+						</div>
+					</div>
 				</div>
-				<div>
-					<img
-						className="h-auto max-w-full rounded-lg object-cover object-center"
-						src={image3}
-						alt="gallery-photo"
-					/>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image3}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>Clean Booth</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 			<div className="grid gap-4">
-				<div>
-					<img
-						className="h-auto max-w-full rounded-lg object-cover object-center"
-						src="https://images.unsplash.com/photo-1552960562-daf630e9278b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
-						alt="gallery-photo"
-					/>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image6}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>Interlock</p>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</div>
