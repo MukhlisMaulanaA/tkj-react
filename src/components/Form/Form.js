8625e3cfdbf716241e7229666529b6f591b9915b
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-10-01 22:12:47 +0700

    update: make form as a component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,69 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+
+const Form = () => {
+	return (
+		<div class="card h-fit max-w-6xl p-5 md:p-12" id="form">
+			<h2 class="mb-4 text-2xl font-bold text-white">Ready to Get Started?</h2>
+			<form>
+				<div class="mb-6">
+					<div class="mx-0 mb-1 sm:mb-4">
+						<label
+							for="name"
+							class="pb-1 text-xs uppercase tracking-wider"
+						></label>
+						<input
+							type="text"
+							id="name"
+							autocomplete="given-name"
+							placeholder="Your name"
+							class="mb-2 w-full rounded border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+							name="name"
+						></input>
+					</div>
+				</div>
+				<div class="mx-0 mb-1 sm:mb-4">
+					<label
+						for="email"
+						class="pb-1 text-xs uppercase tracking-wider"
+					></label>
+					<input
+						type="email"
+						id="email"
+						autocomplete="email"
+						placeholder="Your email address"
+						class="mb-2 w-full rounded border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+						name="email"
+					></input>
+				</div>
+				<div class="mx-0 mb-1 sm:mb-4">
+					<label
+						for="textarea"
+						class="pb-1 text-xs uppercase tracking-wider"
+					></label>
+					<textarea
+						id="textarea"
+						name="textarea"
+						cols="30"
+						rows="5"
+						placeholder="Write your message..."
+						class="mb-2 w-full rounded border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
+					></textarea>
+				</div>
+				<div class="text-center">
+					<Link>
+						<button
+							type="submit"
+							class="w-full bg-white text-blueGray-800 px-6 py-3 font-xl rounded sm:mb-0"
+						>
+							Send Message
+						</button>
+					</Link>
+				</div>
+			</form>
+		</div>
+	);
+};
+
+export default Form;
--- src/views/Landing.js
+++ src/views/Landing.js
@@ -10,6 +10,7 @@ import Products from "components/Products/Products.js";
 import AccordionMenu from "components/Accordion/Accordion.js";
 import Gallery from "components/Gallery/Gallery.js";
 import Customer from "components/Customers/Customer.js";
+import Form from "components/Form/Form.js";
 
 export default function Landing() {
 	return (
@@ -483,67 +484,7 @@ export default function Landing() {
 											</li>
 										</ul>
 									</div>
-									<div class="card h-fit max-w-6xl p-5 md:p-12" id="form">
-										<h2 class="mb-4 text-2xl font-bold text-white">
-											Ready to Get Started?
-										</h2>
-										<form>
-											<div class="mb-6">
-												<div class="mx-0 mb-1 sm:mb-4">
-													<label
-														for="name"
-														class="pb-1 text-xs uppercase tracking-wider"
-													></label>
-													<input
-														type="text"
-														id="name"
-														autocomplete="given-name"
-														placeholder="Your name"
-														class="mb-2 w-full rounded border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
-														name="name"
-													></input>
-												</div>
-											</div>
-											<div class="mx-0 mb-1 sm:mb-4">
-												<label
-													for="email"
-													class="pb-1 text-xs uppercase tracking-wider"
-												></label>
-												<input
-													type="email"
-													id="email"
-													autocomplete="email"
-													placeholder="Your email address"
-													class="mb-2 w-full rounded border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
-													name="email"
-												></input>
-											</div>
-											<div class="mx-0 mb-1 sm:mb-4">
-												<label
-													for="textarea"
-													class="pb-1 text-xs uppercase tracking-wider"
-												></label>
-												<textarea
-													id="textarea"
-													name="textarea"
-													cols="30"
-													rows="5"
-													placeholder="Write your message..."
-													class="mb-2 w-full rounded border border-gray-400 py-2 pl-2 pr-4 shadow-md dark:text-gray-300 sm:mb-0"
-												></textarea>
-											</div>
-											<div class="text-center">
-												<Link>
-													<button
-														type="submit"
-														class="w-full bg-white text-blueGray-800 px-6 py-3 font-xl rounded sm:mb-0"
-													>
-														Send Message
-													</button>
-												</Link>
-											</div>
-										</form>
-									</div>
+									<Form />
 								</div>
 							</div>
 						</div>
