c5b3e1f1cabece0a55780b474f15cd60f4d42cf8
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-10-03 19:51:53 +0700

    update: embed gmaps in footer

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -59,8 +59,8 @@ export default function Footer() {
 							</div>
 						</div>
 						<div className="w-full lg:w-6/12 px-4">
-							<div className="flex flex-wrap items-top mb-6">
-								<div className="w-full lg:w-4/12 px-4 ml-auto">
+							<div className="flex flex-wrap items-top mb-6 justify-end">
+								<div className="w-full lg:w-4/12 px-4">
 									<span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
 										Useful Links
 									</span>
@@ -95,7 +95,8 @@ export default function Footer() {
 									<span className="block uppercase text-blueGray-500 text-sm font-semibold mb-2">
 										Other Resources
 									</span>
-                  
+                  <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d247.9095184674659!2d107.03085235320026!3d-6.190609839415325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e6989faa19c4b9f%3A0x7e8fcebbc346f3d0!2sTKJ!5e0!3m2!1sid!2sid!4v1693321684679!5m2!1sid!2sid" width="200" height="200"  allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade">
+                  </iframe>
 								</div>
 							</div>
 						</div>
