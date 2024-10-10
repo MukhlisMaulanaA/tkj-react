bcf34f0fd18c8e53db4a3eb08dd71d34297c755f
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-10-10 22:04:06 +0700

    fix: margin top in profile page

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -56,7 +56,7 @@ export default function Profile() {
 											/>
 										</div>
 									</div>
-									<div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
+									{/* <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
 										<div className="py-6 px-3 mt-32 sm:mt-0">
 											<button
 												className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
@@ -93,9 +93,9 @@ export default function Profile() {
 												</span>
 											</div>
 										</div>
-									</div>
+									</div> */}
 								</div>
-								<div className="text-center mt-12">
+								<div className="text-center mt-32">
 									<h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700">
 										PT. Tanjung Karya Jaya
 									</h3>
