fe2871705dcb07f66d22ffaa2d5c12b38e547ae0
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-10-10 21:42:54 +0700

    fix: href contact in footer

--- src/components/Footers/Footer.js
+++ src/components/Footers/Footer.js
@@ -85,7 +85,7 @@ export default function Footer() {
 										<li>
 											<a
 												className="text-blueGray-600 hover:text-blueGray-800 font-semibold block pb-2 text-sm"
-												href="#contact"
+												href="/#contact"
 											>
 												Contact
 											</a>
