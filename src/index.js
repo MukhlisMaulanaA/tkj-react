be920120a75810863d21b081e104e566375f5841
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-10-10 21:57:59 +0700

    fix: reposition tag <Analytics />

--- src/index.js
+++ src/index.js
@@ -19,7 +19,6 @@ import Profile from "views/Profile.js";
 import Product from "views/Product.js";
 // import Index from "views/Index.js";
 
-<Analytics />
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
@@ -34,6 +33,7 @@ ReactDOM.render(
       {/* add redirect for first page */}
       <Redirect from="*" to="/" exact component={Landing}/>
     </Switch>
+    <Analytics />,
   </BrowserRouter>,
   document.getElementById("root")
 );
