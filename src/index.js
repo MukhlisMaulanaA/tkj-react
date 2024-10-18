f5227492bcf48f884ade5e843ac44ac4e9e66e07
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-10-18 20:54:51 +0700

    update: cleaning unnecessary comments in index.js

--- src/index.js
+++ src/index.js
@@ -4,15 +4,7 @@ import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import { Analytics } from "@vercel/analytics/react"
 
 import "@fortawesome/fontawesome-free/css/all.min.css";
-// import "assets/styles/tailwind.css";
 import "assets/styles/index.css";
-
-// layouts
-
-// import Admin from "layouts/Admin.js";
-// import Auth from "layouts/Auth.js";
-
-// views without layouts
 
 import Landing from "views/Landing.js"; 
 import Profile from "views/Profile.js";
@@ -21,15 +13,9 @@ import Product from "views/Product.js";
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
-      {/* add routes with layouts */}
-      {/* <Route path="/admin" component={Admin} /> */}
-      {/* <Route path="/auth" component={Auth} /> */}
-      {/* add routes without layouts */}
       <Route path="/" exact component={Landing} />
       <Route path="/profile" exact component={Profile} />
       <Route path="/products" exact component={Product} />
-      {/* <Route path="/" exact component={Index} /> */}
-      {/* add redirect for first page */}
       <Redirect from="*" to="/" exact component={Landing}/>
     </Switch>
     <Analytics />
