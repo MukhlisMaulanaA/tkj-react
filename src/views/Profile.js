977e587195556ace9c66928862b634eb20ee0027
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2024-10-21 22:41:22 +0700

    update: replace email contact in profile section and contact form using EmailJS

--- src/views/Profile.js
+++ src/views/Profile.js
@@ -56,44 +56,6 @@ export default function Profile() {
 											/>
 										</div>
 									</div>
-									{/* <div className="w-full lg:w-4/12 px-4 lg:order-3 lg:text-right lg:self-center">
-										<div className="py-6 px-3 mt-32 sm:mt-0">
-											<button
-												className="bg-lightBlue-500 active:bg-lightBlue-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-												type="button"
-											>
-												Connect
-											</button>
-										</div>
-									</div>
-									<div className="w-full lg:w-4/12 px-4 lg:order-1">
-										<div className="flex justify-center py-4 lg:pt-4 pt-8">
-											<div className="mr-4 p-3 text-center">
-												<span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-													22
-												</span>
-												<span className="text-sm text-blueGray-400">
-													Friends
-												</span>
-											</div>
-											<div className="mr-4 p-3 text-center">
-												<span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-													10
-												</span>
-												<span className="text-sm text-blueGray-400">
-													Photos
-												</span>
-											</div>
-											<div className="lg:mr-4 p-3 text-center">
-												<span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">
-													89
-												</span>
-												<span className="text-sm text-blueGray-400">
-													Comments
-												</span>
-											</div>
-										</div>
-									</div> */}
 								</div>
 								<div className="text-center mt-32">
 									<h3 className="text-4xl font-semibold leading-normal mb-2 text-blueGray-700">
@@ -116,8 +78,8 @@ export default function Profile() {
 										</div>
 										<div className="mb-2 text-blueGray-600 hover:text-blueGray-800">
 											<i className="fas fa-envelope mr-2 text-lg text-blueGray-400"></i>
-											<a href="mailto:tkjilhamjawaz@gmail.com" target="_blank">
-												tkjilhamjawaz@gmail.com
+											<a href="mailto:pttanjungkaryajaya.office@gmail.com" target="_blank">
+                      pttanjungkaryajaya.office@gmail.com
 											</a>
 										</div>
 										<div className="mb-2 text-blueGray-600 hover:text-blueGray-800">
