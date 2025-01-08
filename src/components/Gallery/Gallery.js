df34715f79e7919b72353c6e584e26c3d5f01909
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2025-01-08 23:14:05 +0700

    update: create layouts for additional items later

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -125,6 +125,119 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
+      
+
+			<div className="grid gap-4">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image5}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>Pas Box</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+      <div className="flex flex-col justify-between">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image1}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Honda Precision Parts Manufacturing
+								</h3>
+								<p>Pembuatan Akrilik</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image2}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Honda Precision Parts Manufacturing
+								</h3>
+								<p>Pembuatan Akrilik</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			
+			<div className="grid gap-4">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image6}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>Interlock</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+      <div className="flex flex-col justify-between">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image4}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>AHU</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image3}
+							alt="gallery-photo"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. Daewong Biologic Indonesia
+								</h3>
+								<p>Clean Booth</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 			<div className="relative group">
 				<div>
 					<img
@@ -193,6 +306,7 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
+      
 		</div>
 	);
 };
