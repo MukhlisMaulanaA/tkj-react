43bccbf25646536793cf04e57bf99d1ecd823aa7
MukhlisMaulanaA <mukhlis.maulanaalf@gmail.com>
2025-10-19 00:47:52 +0700

    update: adding new items to gallery section

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -16,6 +16,14 @@ import image14 from "assets/img/tower-protelindo1.jpg";
 import image15 from "assets/img/tower-protelindo2.jpg";
 import image16 from "assets/img/tower-protelindo3.jpg";
 import image17 from "assets/img/tower-protelindo4.jpg";
+import image18 from "assets/img/pipa-pt-kaneta1.jpg";
+import image19 from "assets/img/pipa-pt-kaneta2.jpg";
+import image20 from "assets/img/ducting-pt-sy-tech1.jpg";
+import image21 from "assets/img/pipa-pt-kaneta3.jpg";
+import image22 from "assets/img/pipa-pt-kaneta4.jpg";
+import image23 from "assets/img/ducting-pt-sy-tech2.jpg";
+
+
 const MasonryGridGallery = () => {
 	return (
 		<div className="grid grid-cols-2 gap-4 md:grid-cols-4">
@@ -26,7 +34,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image1}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -44,7 +52,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image2}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -64,7 +72,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image5}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -84,7 +92,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image4}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -102,7 +110,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image3}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -122,7 +130,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image6}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -135,7 +143,6 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
-      
 
 			<div className="grid gap-4">
 				<div className="relative group">
@@ -144,33 +151,29 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image11}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
-								<h3 className="font-bold bottom-0 text-left">
-									Telkomsel
-								</h3>
+								<h3 className="font-bold bottom-0 text-left">Telkomsel</h3>
 								<p>Pemasangan Baterai Lithium</p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-      <div className="flex flex-col justify-between">
+			<div className="flex flex-col justify-between">
 				<div className="relative group">
 					<div>
 						<img
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image12}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
-								<h3 className="font-bold bottom-0 text-left">
-									Telkomsel
-								</h3>
+								<h3 className="font-bold bottom-0 text-left">Telkomsel</h3>
 								<p>Pemasangan Baterai Lithium</p>
 							</div>
 						</div>
@@ -182,20 +185,18 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image13}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
-								<h3 className="font-bold bottom-0 text-left">
-									Telkomsel
-								</h3>
+								<h3 className="font-bold bottom-0 text-left">Telkomsel</h3>
 								<p>Pemasangan Baterai Lithium</p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-			
+
 			<div className="grid gap-4">
 				<div className="relative group">
 					<div>
@@ -203,7 +204,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image7}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -216,14 +217,14 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
-      <div className="flex flex-col justify-between">
+			<div className="flex flex-col justify-between">
 				<div className="relative group">
 					<div>
 						<img
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image8}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -241,7 +242,7 @@ const MasonryGridGallery = () => {
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image10}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -254,14 +255,14 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
-      <div className="grid gap-4">
+			<div className="grid gap-4">
 				<div className="relative group">
 					<div>
 						<img
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image14}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -274,14 +275,14 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
-      <div className="grid gap-4">
+			<div className="grid gap-4">
 				<div className="relative group">
 					<div>
 						<img
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image15}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -294,14 +295,14 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
-      <div className="grid gap-4">
+			<div className="grid gap-4">
 				<div className="relative group">
 					<div>
 						<img
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image16}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -314,14 +315,14 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
-      <div className="grid gap-4">
+			<div className="grid gap-4">
 				<div className="relative group">
 					<div>
 						<img
 							className="h-auto max-w-full rounded-lg object-cover object-center"
 							src={image17}
 							alt="gallery"
-              loading="lazy"
+							loading="lazy"
 						/>
 						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
 							<div className="w-full text-sm absolute text-white bottom-0 p-4">
@@ -334,8 +335,114 @@ const MasonryGridGallery = () => {
 					</div>
 				</div>
 			</div>
-      
-			
+
+			<div className="flex flex-col justify-between gap-2">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image18}
+							alt="gallery"
+							loading="lazy"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">PT. Kaneta</h3>
+								<p>Pipa</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image19}
+							alt="gallery"
+							loading="lazy"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">PT. Kaneta</h3>
+								<p>Pipa</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="grid gap-4">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image20}
+							alt="gallery"
+							loading="lazy"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">PT. SY Tech</h3>
+								<p>Ducting</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="flex flex-col justify-between">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image21}
+							alt="gallery"
+							loading="lazy"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">PT. Kaneta</h3>
+								<p>Pipa</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image22}
+							alt="gallery"
+							loading="lazy"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">PT. Kaneta</h3>
+								<p>Pipa</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+			<div className="grid gap-4">
+				<div className="relative group">
+					<div>
+						<img
+							className="h-auto max-w-full rounded-lg object-cover object-center"
+							src={image23}
+							alt="gallery"
+							loading="lazy"
+						/>
+						<div className="absolute inset-0 bg-gradient-to-t-black flex items-center justify-center opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+							<div className="w-full text-sm absolute text-white bottom-0 p-4">
+								<h3 className="font-bold bottom-0 text-left">
+									PT. SY Tech
+								</h3>
+								<p>Ducting</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+
       
 		</div>
 	);
